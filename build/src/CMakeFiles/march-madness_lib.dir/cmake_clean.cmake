f3da194a79ce1813690c2015ecba768e02bb868f
Patrick Hesse <phesse001@csbsju.edu>
2020-07-20 18:00:57 -0500

    first commit

---
+++ build/src/CMakeFiles/march-madness_lib.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/march-madness_lib.dir/Simulator.cpp.o"
+  "CMakeFiles/march-madness_lib.dir/Game.cpp.o"
+  "CMakeFiles/march-madness_lib.dir/Team.cpp.o"
+  "libmarch-madness_lib.pdb"
+  "libmarch-madness_lib.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/march-madness_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/src/CMakeFiles/march-madness_lib.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "libmarch-madness_lib.a"
+)
